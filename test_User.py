5c6a78da42d80b1c923a6c634854603f92061e40
Filipe André <flipeandre12@gmail.com>
2023-08-23 12:06:15 -0300

    test: removing a user

--- test_User.py
+++ test_User.py
@@ -14,3 +14,11 @@ def test_failing_creating_a_new_user_in_list():
     result = new_list.insert_user(user)
 
     assert 'Not a user, could not add in list !' == result
+
+def test_removing_a_user_in_list():
+    user = User('Filipe', 19, '06/03/2004')
+    new_list = ListOfUsers()
+    new_list.insert_user(user)
+    result = new_list.remove_user(0) # remove a partir do index do usuário
+
+    assert 'User added in the list !' == result
