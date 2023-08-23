65911f9a137a6ee82985d65e6f74fc42208d4d1e
Filipe André <flipeandre12@gmail.com>
2023-08-23 11:47:52 -0300

    feat: add first test

---
+++ test_User.py
@@ -0,0 +1,6 @@
+def test_creating_a_new_user_in_list():
+    user = User('Filipe', 19, '06/03/2004')
+    new_list = ListOfUsers()
+    result = new_list.insert_user(user)
+
+    assert 'User added in the list !' == result
