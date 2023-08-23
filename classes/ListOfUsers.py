ef5c176f9c01e5c510387370ee68cbe3bcdfad5c
Filipe André <flipeandre12@gmail.com>
2023-08-23 12:03:37 -0300

    test: failing insert ✔️

--- classes/ListOfUsers.py
+++ classes/ListOfUsers.py
@@ -1,8 +1,11 @@
+from classes.User import User
 class ListOfUsers:
 
     def __init__(self) -> None:
         self.list = []
 
     def insert_user(self, user):
+        if not isinstance(user, User):
+            return 'Not a user, could not add in list !'
         self.list.append(user)
         return "User added in the list !"
