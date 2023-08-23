641fb060620007273322ad219a3b17fc9e54f165
Filipe André <flipeandre12@gmail.com>
2023-08-23 11:59:33 -0300

    test: new test(failing insert)

--- test_User.py
+++ test_User.py
@@ -7,3 +7,10 @@ def test_creating_a_new_user_in_list():
     result = new_list.insert_user(user)
 
     assert 'User added in the list !' == result
+
+def test_failing_creating_a_new_user_in_list():
+    user = 1
+    new_list = ListOfUsers()
+    result = new_list.insert_user(user)
+
+    assert 'Not a user, could not add in list !' == result
